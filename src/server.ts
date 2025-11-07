018bd0cba6143d9fb9264312ebb2718e78e087f1
Alexey Yanchuk <yanchuk@users.noreply.github.com>
2025-11-07 13:38:55 +0100

    fix hint for sandbox email

--- src/server.ts
+++ src/server.ts
@@ -77,7 +77,7 @@ const tools = [
     inputSchema: sendSandboxEmailSchema,
     handler: sendSandboxEmail,
     annotations: {
-      destructiveHint: true,
+      destructiveHint: false,
     },
   },
 ];
